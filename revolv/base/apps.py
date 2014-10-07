48c2ce7fc2cae67cc83702d43221af48246fc749
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-07 14:04:56 -0700

    app config and signals for user base app

---
+++ revolv/base/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class RevolvBaseConfig(AppConfig):
+    name = 'base'
+    verbose_name = "Revolv Base User"
+
+    def ready(self):
+        import revolv.base.signals
+        revolv.base.signals
