361ca2d46ee115536467494614612d098c1980bb
Eric Shen <email.ericshen@gmail.com>
2014-10-07 00:21:50 -0700

    Updated test to see if storing and retrieving work for the project model

--- revolv/project/tests.py
+++ revolv/project/tests.py
@@ -8,66 +8,23 @@ class ProjectTests(TestCase):
 	"""Project model tests."""
 
 	def test_construct(self):
-		now = datetime.datetime.now()
-		today = datetime.date.today()
-		project = Project(
+		yesterday = datetime.date.today() - datetime.timedelta(days=1)
+		# Note: there is a runtime warning of using a datetime
+		# that is not timezone aware
+		tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+		Project.objects.create(
 			funding_goal=50.0,
 			title="Hello",
 			video_url="https://www.youtube.com/watch?v=9bZkp7q19f0",
 			impact_power=50.5,
 			location="Berkeley",
-			end_date=now,
+			end_date=tomorrow,
 			mission_statement="We do solar!",
 			cover_photo="http://static.fjcdn.com/pictures/Why_05304a_2913881.jpg",
-			org_start_date=today,
+			org_start_date=yesterday,
 			actual_energy=25.5,
 			amount_repaid=29.25,
 		)
-		self.assertEquals(
-			project.funding_goal,
-			50.0
-		)
-		self.assertEquals(
-			project.title,
-			"Hello"
-		)
-		self.assertEquals(
-			project.video_url,
-			"https://www.youtube.com/watch?v=9bZkp7q19f0"		
-		)
-		self.assertEquals(
-			project.impact_power,
-			50.5
-		)
-		self.assertEquals(
-			project.location,
-			"Berkeley"
-		)
-		self.assertEquals(
-			project.end_date,
-			now
-		)
-		self.assertEquals(
-			project.project_status,
-			'PR'
-		)
-		self.assertEquals(
-			project.mission_statement,
-			"We do solar!"
-		)
-		self.assertEquals(
-			project.cover_photo,
-			"http://static.fjcdn.com/pictures/Why_05304a_2913881.jpg"
-		)
-		self.assertEquals(
-			project.org_start_date,
-			today
-		)
-		self.assertEquals(
-			project.actual_energy,
-			25.5
-		)
-		self.assertEquals(
-			project.amount_repaid,
-			29.25
-		)
+		testProject = Project.objects.get(title="Hello")
+		self.assertEqual(testProject.mission_statement, "We do solar!")
+		self.assertEqual(testProject.impact_power, 50.5)
