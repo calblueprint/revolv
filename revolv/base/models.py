6d3776c84197979529c20c5d1f8e1b2d2793fd83
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-07 13:31:58 -0700

    add facebook profile model

--- revolv/base/models.py
+++ revolv/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_facebook.models import FacebookModel
+from revolv import settings
 
-# Create your models here.
+
+class RevolvUserProfile(FacebookModel):
+    """
+    A simple wrapper around django-facebook's FacebookModel, which contains
+    Facebook information like name, etc. RevolvUserProfile ties a FacebookModel
+    and a django auth.User model together, so that we can use both Facebook
+    and non-Facebook user profiles.
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
