25b030afd3422be5f4c8f1307c8809499a789974
Eric Shen <email.ericshen@gmail.com>
2014-10-06 23:45:10 -0700

    Added project to included apps and cleaned up project models.py code

--- revolv/project/models.py
+++ revolv/project/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    # or an integer field if we don't allow cents
-    funding_goal = models.DecimalField(decimal_places=2)
+    ACCEPTED = 'AC'
+    PROPOSED = 'PR'
+    COMPLETED = 'CO'
+    BUILDING = 'BU'
+    PROJECT_STATUS_CHOICES = (
+        (ACCEPTED, 'Accepted'),
+        (PROPOSED, 'Proposed'),
+        (COMPLETED, 'Completed'),
+        (BUILDING, 'Building'),
+    )
+    funding_goal = models.DecimalField(max_digits=15, decimal_places=2)
     title = models.CharField(max_length=255)
     video_url = models.URLField(max_length=255)
     # power output of array in kilowatts
@@ -14,18 +23,22 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     end_date = models.DateTimeField()
-    project_status = models.CharField(max_length=255)
+    project_status = models.CharField(
+        max_length=2,
+        choices=PROJECT_STATUS_CHOICES,
+        default=PROPOSED
+    )
     mission_statement = models.CharField(max_length=5000)
     # or an ImageField if we let them upload images
     cover_photo = models.URLField(max_length=255)
     org_start_date = models.DateField()
-    # donor = models.ManyToManyField(Donor)
+    
     # commented out until Donor model is implemented
+    # donor = models.ManyToManyField(Donor)
 
-    # ambassador = models.ForeignKey(Ambassador)
     # commented out until Ambassador model is implemented
+    # ambassador = models.ForeignKey(Ambassador)
 
     # energy produced in kilowatt hours
     actual_energy = models.FloatField()
-    # or an integer field if we don't allow cents
-    amount_repaid = models.DecimalField(decimal_places=2)
+    amount_repaid = models.DecimalField(max_digits=15, decimal_places=2)
