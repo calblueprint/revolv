aba0baae5c41e7db1cd155af20c34d62b7a228f9
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-07 14:13:33 -0700

    add user profile test

--- revolv/base/tests.py
+++ revolv/base/tests.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from revolv.base.models import RevolvUserProfile
+
 
 
 class SmokeTestCase(TestCase):
     def test_works(self):
         """Test that the test framework works."""
         self.assertEqual(1, 1)
+
+
+class UserAuthTestCase(TestCase):
+    def test_user_profile_sync(self):
+        test_user = User.objects.create("John", "john@example.com", "password")
+        profile = RevolvUserProfile.objects.filter(user=test_user).first()
+        self.assertIsNotNone(profile)
+        self.assertIs(profile, test_user.revolvuserprofile)
