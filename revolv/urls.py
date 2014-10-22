d5a3bab48c29836b4753f587cc6a01af05192fe8
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-22 13:39:42 -0700

    add signin view skeleton

--- revolv/urls.py
+++ revolv/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
-from revolv.base.views import HomePageView
+from revolv.base.views import HomePageView, SignInView
 
 urlpatterns = patterns('',
                        # Examples:
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
                        (r'^facebook/', include('django_facebook.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', HomePageView.as_view(), name='home'),
+                       url(r'^signin/$', SignInView.as_view(), name='signin'),
                        )
