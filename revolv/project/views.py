a2f66b5d4667eb0ce0840b8ce6dbca90c523bc7c
Eric Shen <email.ericshen@gmail.com>
2014-10-25 16:57:35 -0700

    Update project create view so it works again

--- revolv/project/views.py
+++ revolv/project/views.py
@@ -18,7 +18,7 @@ class CreateProjectView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(CreateProjectView, self).get_context_data(**kwargs)
-        context['action'] = reverse('project-create')
+        context['action'] = reverse('project-new')
         return context
 
 
