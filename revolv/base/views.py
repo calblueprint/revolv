1295c0b7e75db1f4dda3c62e796c66653960c7f3
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-29 13:59:54 -0700

    untested implementation of django auth signup view

--- revolv/base/views.py
+++ revolv/base/views.py
@@ -49,7 +49,32 @@ class SignInView(TemplateView):
         return context
 
 
-class LoginView(FormView):
+class RedirectToSigninOrHomeMixin(object):
+    """
+    Mixin that detects if a page was requested with method="GET", and redirects
+    to the signin page if so. Also, if posted to with an already authenticated
+    user, will redirect to the homepage instead.
+
+    This is useful both for the login and signup endpoints.
+    """
+    @method_decorator(sensitive_post_parameters(
+        "password", "password1", "password2"
+    ))
+    def dispatch(self, request, *args, **kwargs):
+        # don't allow rendering a form page with a GET request to this view:
+        # instead, redirect to the signin page
+        if self.request.method == "GET":
+            return redirect("signin")
+        # if the user is already logged in and tries to log in again, just
+        # redirect them to the home page.
+        if request.user.is_authenticated():
+            return redirect("home")
+        return super(RedirectToSigninOrHomeMixin, self).dispatch(
+            request, *args, **kwargs
+        )
+
+
+class LoginView(RedirectToSigninOrHomeMixin, FormView):
     """
     Login endpoint: checks the data from the received request against
     django.contrib.auth.forms.AuthenticationForm and logs in the user if
@@ -67,17 +92,8 @@ class LoginView(FormView):
 
     @method_decorator(sensitive_post_parameters('password'))
     def dispatch(self, request, *args, **kwargs):
-        # don't allow rendering a form page with a GET request to this view:
-        # instead, redirect to the signin page
-        if self.request.method == "GET":
-            return redirect("signin")
-        # if the user is already logged in and tries to log in again, just
-        # redirect them to the home page.
-        if request.user.is_authenticated():
-            return redirect("home")
-        else:
-            self.next_url = request.POST.get("next", "home")
-            return super(LoginView, self).dispatch(request, *args, **kwargs)
+        self.next_url = request.POST.get("next", "home")
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
 
     def form_invalid(self, form):
         return redirect("signin")
@@ -86,3 +102,28 @@ class LoginView(FormView):
         """Log the user in and redirect them to the supplied next page."""
         auth_login(self.request, form.get_user())
         return redirect(self.next_url)
+
+
+class SignupView(RedirectToSigninOrHomeMixin, FormView):
+    """
+    Signup endpoint: processes the signup form and signs the user up (and logs
+    them in). Note that the sign up with facebook functionality is entirely
+    different: this is only for the regular django auth signup flow.
+
+    Http verbs:
+        GET: redirect to signin page
+        POST: check post params against form, redirect to signin page if the
+            form is not valid.
+
+    Todo(noah): add in form error messages for the signin page
+    """
+    form_class = SignupForm
+
+    def form_invalid(self, form):
+        return redirect("signin")
+
+    def form_valid(self, form):
+        form.save()
+        # log in the newly created user model. if there is a problem, error
+        auth_login(self.request, form.ensure_authenticated_user())
+        return redirect("home")
