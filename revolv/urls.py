44942f1286f1378a3cc63acda903d70ef4a6c4d5
vivekraghuram <VV9295@YAHOO.COM>
2014-10-30 19:17:42 -0700

    Fix syntax error in urls.py

--- revolv/urls.py
+++ revolv/urls.py
@@ -2,16 +2,16 @@ import django.contrib.auth.views as auth_views
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from revolv.base.views import HomePageView, LoginView, SignInView, SignupView
-from revolv.project.views import (CreateProjectView, ProjectView,
-                                  UpdateProjectView)
 
 urlpatterns = patterns('',
-    (r'^facebook/', include('django_facebook.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^project/', include('revolv.project.urls', namespace='project'))
-   url(r'^signin/$', SignInView.as_view(), name='signin'),
-   url(r'^login/$', LoginView.as_view(), name='login'),
-   url(r'^signup/$', SignupView.as_view(), name='login'),
-   url(r'^logout/$', auth_views.logout, {"next_page": "/"}, name='logout'),
-)
+                       (r'^facebook/', include('django_facebook.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', HomePageView.as_view(), name='home'),
+                       url(r'^project/', include('revolv.project.urls',
+                                                 namespace='project')),
+                       url(r'^signin/$', SignInView.as_view(), name='signin'),
+                       url(r'^login/$', LoginView.as_view(), name='login'),
+                       url(r'^signup/$', SignupView.as_view(), name='login'),
+                       url(r'^logout/$', auth_views.logout, {"next_page": "/"},
+                           name='logout'),
+                       )
