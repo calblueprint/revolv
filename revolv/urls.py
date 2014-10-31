176f2ec2c46231b91f4b0f73b0e5a9eaed9b7044
vivekraghuram <VV9295@YAHOO.COM>
2014-10-30 17:27:17 -0700

    Move project urls into app specific urls.py

--- revolv/urls.py
+++ revolv/urls.py
@@ -6,28 +6,12 @@ from revolv.project.views import (CreateProjectView, ProjectView,
                                   UpdateProjectView)
 
 urlpatterns = patterns('',
-                       (r'^facebook/', include('django_facebook.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', HomePageView.as_view(), name='home'),
-                       url(r'^project/create$', CreateProjectView.as_view(),
-                           name='project-new'),
-                       url(r'^project/edit/(?P<pk>\d+)/$',
-                           UpdateProjectView.as_view(),
-                           name='project-edit'),
-                       url(r'^project/(?P<pk>\d+)/$', ProjectView.as_view(),
-                           name='project-view'),
-                       url(r'^signin/$', SignInView.as_view(), name='signin'),
-                       url(r'^login/$', LoginView.as_view(), name='login'),
-                       url(r'^signup/$', SignupView.as_view(), name='login'),
-                       url(
-                           r'^logout/$',
-                           auth_views.logout,
-                           {"next_page": "/"},
-                           name='logout'
-                       ),
-                       )
-
-
-# Examples:
-# url(r'^$', 'revolv.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+    (r'^facebook/', include('django_facebook.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^project/', include('revolv.project.urls', namespace='project'))
+   url(r'^signin/$', SignInView.as_view(), name='signin'),
+   url(r'^login/$', LoginView.as_view(), name='login'),
+   url(r'^signup/$', SignupView.as_view(), name='login'),
+   url(r'^logout/$', auth_views.logout, {"next_page": "/"}, name='logout'),
+)
