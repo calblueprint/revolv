12a5e4be90cf9ed2a629f4c769262ed5271ec956
vivekraghuram <VV9295@YAHOO.COM>
2014-11-05 09:43:16 -0800

    Configure login_required to work

--- revolv/urls.py
+++ revolv/urls.py
@@ -1,20 +1,18 @@
 import django.contrib.auth.views as auth_views
 from django.conf.urls import include, patterns, url
+from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 from revolv.base.views import (DashboardView, HomePageView, LoginView,
                                SignInView, SignupView)
 
 urlpatterns = patterns('',
-                       url(r'^facebook/', include('django_facebook.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', HomePageView.as_view(), name='home'),
-                       url(r'^project/', include('revolv.project.urls',
-                                                 namespace='project')),
-                       url(r'^users/', DashboardView.as_view(),
-                           name='dashboard'),
-                       url(r'^signin/$', SignInView.as_view(), name='signin'),
-                       url(r'^login/$', LoginView.as_view(), name='login'),
-                       url(r'^signup/$', SignupView.as_view(), name='signup'),
-                       url(r'^logout/$', auth_views.logout, {"next_page": "/"},
-                           name='logout'),
-                       )
+    url(r'^facebook/', include('django_facebook.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^project/', include('revolv.project.urls', namespace='project')),
+    url(r'^users/', login_required(DashboardView.as_view()), name='dashboard'),
+    url(r'^signin/$', SignInView.as_view(), name='signin'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^signup/$', SignupView.as_view(), name='signup'),
+    url(r'^logout/$', auth_views.logout, {"next_page": "/"}, name='logout'),
+)
