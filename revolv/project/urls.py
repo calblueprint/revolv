df28f1b8b62790e2828f46291047a677f776e47f
vivekraghuram <VV9295@YAHOO.COM>
2014-11-08 14:40:08 -0800

    Rearrange urls in project.urls

--- revolv/project/urls.py
+++ revolv/project/urls.py
@@ -7,8 +7,8 @@ from revolv.project.views import (CreateProjectDonationView, CreateProjectView,
 urlpatterns = patterns(
     '',
     url(r'^create$', login_required(CreateProjectView.as_view()), name='new'),
-    url(r'^edit/(?P<pk>\d+)/$', login_required(UpdateProjectView.as_view()), name='edit'),
-    url(r'^review/(?P<pk>\d+)/$', login_required(ReviewProjectView.as_view()), name='review'),
+    url(r'^(?P<pk>\d+)/edit$', login_required(UpdateProjectView.as_view()), name='edit'),
+    url(r'^(?P<pk>\d+)/review$', login_required(ReviewProjectView.as_view()), name='review'),
     url(r'^(?P<pk>\d+)/$', ProjectView.as_view(), name='view'),
     url(r'^(?P<pk>\d+)/donate$', CreateProjectDonationView.as_view(), name='donate'),
 )
