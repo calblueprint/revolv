44027534d3f316721607f0c10666b8b22f18a30f
vivekraghuram <VV9295@YAHOO.COM>
2014-11-08 01:28:17 -0800

    Write tests and fix donation manager methods. They might be inefficient

--- revolv/payments/models.py
+++ revolv/payments/models.py
@@ -28,22 +28,25 @@ class DonationManager(models.Manager):
         Simple manager for the donations model
     """
 
-    def donated_projects(self, user, queryset=None):
-        """
-        :return: Projects to which this user has donated
-        """
-        if queryset is None:
-            return self.user_donations(user).value_list('project', flat=True)
-        return queryset.value_list('project', flat=True)
-
     def user_donations(self, user, queryset=None):
         """
         :return: Donations made by this user
         """
         if queryset is None:
             queryset = super(DonationManager, self).get_queryset()
-        donations = queryset.filter(donor=user).order_by('created_at')
+        donations = queryset.filter(payment_transaction__user=user).order_by('created_at')
         return donations
+
+    def donated_projects(self, user, queryset=None):
+        """
+        :return: Projects to which this user has donated
+        """
+        if queryset is None:
+            queryset = self.user_donations(user).select_related("project")
+        projects = []
+        for donation in queryset:
+            projects.append(donation.project)
+        return projects
 
 
 class Donation(models.Model):
@@ -56,14 +59,12 @@ class Donation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     project = models.ForeignKey(Project)
 
-    @property
     def amount(self):
         """
         :return: The amount tied to this donation
         """
         return self.payment_transaction.amount
 
-    @property
     def donor(self):
         """
         :return: The user tied to this donation
--- revolv/payments/tests/models/test_donation.py
+++ revolv/payments/tests/models/test_donation.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.db.models.signals import post_save
 from django.test import TestCase
+from django_facebook.utils import get_user_model
+from revolv.base.signals import create_profile_of_user
 from revolv.payments.models import (Donation, PaymentInstrumentType,
                                     PaymentTransaction)
 from revolv.project.models import Project
@@ -17,3 +21,25 @@ class DonationTest(TestCase):
 
         # Try instantiating the model
         donation = Donation(project=project, payment_transaction=pt)
+
+
+class DonationManagerTest(TestCase):
+
+    def setUp(self):
+        post_save.disconnect(receiver=create_profile_of_user, sender=get_user_model())
+        call_command('loaddata', 'user', 'revolvuserprofile', 'donation', 'payment_transaction', 'project')
+
+    def test_user_donations(self):
+        """Verify that we can find donations by user"""
+        user = User.objects.get(id=1)
+        donations = Donation.objects.user_donations(user)
+        self.assertEqual(len(donations), 2)
+        self.assertEqual(donations[0].amount(), 50)
+        self.assertEqual(donations[1].donor(), user)
+
+    def test_donated_projects(self):
+        """Verify that we can find user donated projects from donations"""
+        user = User.objects.get(id=2)
+        projects = Donation.objects.donated_projects(user)
+        self.assertEqual(len(projects), 1)
+        self.assertEqual(projects[0].org_name, "Comoonity Dairy")
