94dc329b00edab913067559643f1dfbe30db41b0
jaytlin <heyjaylin@gmail.com>
2014-11-14 18:21:37 -0800

    Remove test donated_projects from Donation

--- revolv/payments/tests/models/test_donation.py
+++ revolv/payments/tests/models/test_donation.py
@@ -4,23 +4,7 @@ from django.db.models.signals import post_save
 from django.test import TestCase
 from django_facebook.utils import get_user_model
 from revolv.base.signals import create_profile_of_user
-from revolv.payments.models import (Donation, PaymentInstrumentType,
-                                    PaymentTransaction)
-from revolv.project.models import Project
-
-
-class DonationTest(TestCase):
-
-    def test_donation_create(self):
-        """Verify that we can create a donation."""
-        # Variables for setup
-        user = User(username='user1')
-        project = Project()
-        instrument_type = PaymentInstrumentType(name='test instrument')
-        pt = PaymentTransaction(amount=10.00, user=user, payment_instrument_type=instrument_type)
-
-        # Try instantiating the model
-        donation = Donation(project=project, payment_transaction=pt)
+from revolv.payments.models import Donation
 
 
 class DonationManagerTest(TestCase):
@@ -39,10 +23,3 @@ class DonationManagerTest(TestCase):
         self.assertEqual(len(donations), 2)
         self.assertEqual(donations[0].amount(), 50)
         self.assertEqual(donations[1].donor(), user)
-
-    def test_donated_projects(self):
-        """Verify that we can find user donated projects from donations"""
-        user = User.objects.get(id=2)
-        projects = Donation.objects.donated_projects(user)
-        self.assertEqual(len(projects), 1)
-        self.assertEqual(projects[0].org_name, "Comoonity Dairy")
