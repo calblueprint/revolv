c1067a9b9098e54b0d58eb02e6b322e5f621b782
jaytlin <heyjaylin@gmail.com>
2014-12-02 23:24:34 -0800

    Add beautifulsoup4 to reqs

--- revolv/project/tests.py
+++ revolv/project/tests.py
@@ -98,6 +98,9 @@ class ProjectManagerTests(TestCase):
 
 
 class ScrapeTest(TestCase):
+    """Test that the scrape task runs with no errors,
+        and changes the project's solar data files"""
+
     def test_scrape(self):
         result = scrape.delay()
         self.assertTrue(result.successful())
