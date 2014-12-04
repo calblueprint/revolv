15632c762e6ff64751c0da1cfcf5f6893a67cbf9
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-12-04 00:07:08 -0800

    background circle + partial circle for project homepage badges

--- revolv/static/js/base/home.js
+++ revolv/static/js/base/home.js
@@ -1,11 +1,7 @@
-$(document).ready(function () {
-    // method of drawing partial circles in d3 open source from http://jsfiddle.net/Wexcode/CrDUy/
-    var radius = 100,
-        padding = 10,
+var drawD3PartialTriangle = function (destination, classes, radius, padding, partial) {
+    var points = 100,
+        pointsToDraw = Math.floor(points * partial),
         radians = 2 * Math.PI;
-
-    var dimension = (2 * radius) + (2 * padding),
-        points = 50;
 
     var angle = d3.scale.linear()
         .domain([0, points-1])
@@ -17,14 +13,38 @@ $(document).ready(function () {
         .radius(radius)
         .angle(function(d, i) { return angle(i); });
 
-    var svg = d3.select("body").append("svg")
+    var result = destination.append("path").datum(d3.range(pointsToDraw));
+
+    classes.forEach(function (cls) {
+        var currentClass = result.attr("class");
+        if (currentClass) {
+            result.attr("class", currentClass + " " + cls);
+        } else {
+            result.attr("class", cls);
+        }
+    });
+
+    result.attr("d", line)
+        .attr("transform", "translate(" + (radius + padding) + ", " + (radius + padding) + ")");
+};
+
+$(document).ready(function () {
+    // method of drawing partial circles in d3 open source from http://jsfiddle.net/Wexcode/CrDUy/
+    var radius = 100,
+        padding = 10,
+        radians = 2 * Math.PI;
+
+    var dimension = (2 * radius) + (2 * padding);
+
+    var svg = d3.select(".project-badges-container").append("svg")
         .attr("width", dimension)
         .attr("height", dimension)
     .append("g");
 
-    svg.append("path").datum(d3.range(points-24))
-        .attr("class", "line")
-        .attr("d", line)
-        .attr("transform", "translate(" + (radius + padding) + ", " + (radius + padding) + ")");
+    var circleGrouping = svg.append("g").attr("class", "project-badge-circle-grouping");
+    var partialGrouping = svg.append("g").attr("class", "project-badge-partial-grouping");
+
+    drawD3PartialTriangle(circleGrouping, ["project-badge-circle"], radius, padding, 1);
+    drawD3PartialTriangle(partialGrouping, ["project-badge-line"], radius, padding, 0.75);
 
 });
