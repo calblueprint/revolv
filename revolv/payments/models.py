7cb81ae21c009e4b843b3ed8aa0dc792a14aa349
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-02-23 19:33:03 -0800

    more verbose docstrings are always good

--- revolv/payments/models.py
+++ revolv/payments/models.py
@@ -29,7 +29,26 @@ class PaymentInstrumentTypeManager(models.Manager):
 
 class PaymentInstrumentType(models.Model):
     """
-        Abstraction for a payment instrument. (e.g. Paypal)
+        Abstraction for a payment instrument. A payment instrument is defined as
+        anything that can be used to create a payment (for example, paypal or any
+        other payment service). Types of payment that the RE-volv app currently
+        supports are as follows:
+
+        1. "paypal": a payment that was made via a credit card processed through
+           the paypal REST sdk. TODO: the distinction between an actual payment
+           through a paypal account and a payment made with credit card but processed
+           by paypal.
+        2. "check": a record or a payment that was made directly to a RE-volv
+           administrator via paper check. These must be entered into the app via
+           the UI in the administrator dashboard page in order for our database to
+           know about them.
+        3. "repayment" (DEPRECATED): this represents a payment that was made from a
+           community organization that RE-volv has already funded and is a return on
+           that solar panel investment. These are available to be reinvested.
+        4. "reinvestment": this is a payment that was allocated to a new project as
+           part the revolving funding model - the money originally came from an organic
+           donation to an already completed project, and then the community repayed
+           revolv the money that is represented in the Payment that has this PaymentInstrumentType.
     """
     name = models.CharField(max_length=80, unique=True)
 
