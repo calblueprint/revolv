810d37e20e69e04cf241a33d28a9d2c715eb3436
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-02-23 19:44:39 -0800

    clean up payment service tests

--- revolv/payments/tests/services/test_payment_service.py
+++ revolv/payments/tests/services/test_payment_service.py
@@ -1,23 +1,12 @@
 import mock
-from django.core.management import call_command
-from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.test import TestCase
-from django_facebook.utils import get_user_model
-from revolv.base.models import RevolvUserProfile
-from revolv.base.signals import create_profile_of_user
 from revolv.payments.models import PaymentInstrumentType
 from revolv.payments.services import PaymentService, PaymentServiceException
-from revolv.project.models import Project
+from revolv.project.tests import CreateTestProjectMixin
 
 
-class PaymentServiceTest(TestCase):
-
-    def setUp(self):
-        post_save.disconnect(receiver=create_profile_of_user, sender=get_user_model())
-        call_command('loaddata', 'user', 'revolvuserprofile', 'project')
-
-    def tearDown(self):
-        post_save.connect(create_profile_of_user, sender=get_user_model())
+class PaymentServiceTest(TestCase, CreateTestProjectMixin):
 
     @mock.patch('revolv.payments.services.PaymentService.check_valid_amount')
     @mock.patch('revolv.payments.services.PaymentService.check_valid_payment_instrument')
@@ -27,14 +16,14 @@ class PaymentServiceTest(TestCase):
         mock_amount.return_value = True
         mock_instrument.return_value = True
 
-        # Mock out a payment instrument
+        # Mock out a payment instrument with paypal by default
         instrument_type = mock.PropertyMock(
-            return_value=PaymentInstrumentType.objects.get(id=1),
+            return_value=PaymentInstrumentType.objects.get_paypal(),
         )
         payment_instrument = mock.Mock()
         type(payment_instrument).type = instrument_type
-        project = Project.objects.get(id=1)
-        user = RevolvUserProfile.objects.get(id=1)
+        project = self.create_test_project()
+        user = (User.objects.create_user(username="john", password="doe")).revolvuserprofile
 
         # Make the payment
         PaymentService.create_payment(
@@ -60,7 +49,7 @@ class PaymentServiceTest(TestCase):
     def test_check_valid_payment_instrument(self):
         """Verify that we can get a valid payment instrument type."""
         # Mock out a payment instrument
-        instrument_type = PaymentInstrumentType.objects.get(id=1)
+        instrument_type = PaymentInstrumentType.objects.get_paypal()
 
         # Check that it returns True
         self.assertTrue(PaymentService.check_valid_payment_instrument(instrument_type))
