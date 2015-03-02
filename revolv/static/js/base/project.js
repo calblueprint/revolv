3b744cff3da1903491c3964299e6646881dc482a
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-03-02 07:51:07 -0800

    fixed up the project page and updated ambassador and donor dashboards

--- revolv/static/js/base/project.js
+++ revolv/static/js/base/project.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
 	$("#donate-button").click(function(){
 		$(this).find('a').click();
 	});
+
+	$('.tab-item').click(function(){
+		$('.tab').removeClass("active");
+		$('.tab-item').removeClass("active");
+		
+		$(this).addClass("active");
+		var ref = $(this).find('a').attr('href');
+		console.log(ref);
+		$(ref).addClass('active');
+	});
 });
