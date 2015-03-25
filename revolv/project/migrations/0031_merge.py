7b4a4115dc91e6b5aded108f8621f76519d6ce4b
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-03-25 16:55:24 -0700

    added migration files

---
+++ revolv/project/migrations/0031_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('project', '0029_project_tagline'),
+        ('project', '0030_auto_20150312_0014'),
+    ]
+
+    operations = [
+    ]
