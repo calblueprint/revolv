e33ecd556e674b82740387f248f2a8aba9113f70
Andy Qin <andrewlqin@gmail.com>
2015-03-25 21:02:35 -0700

    made migrations

---
+++ revolv/project/migrations/0035_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('project', '0030_auto_20150317_0215'),
+        ('project', '0034_auto_20150317_2310'),
+    ]
+
+    operations = [
+    ]
