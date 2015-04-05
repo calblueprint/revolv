242e869de30c62304a3a0e44d52ea9b2afed4883
Eric Shen <email.ericshen@gmail.com>
2015-04-05 13:58:14 -0700

    Html email for monthly donation email

--- revolv/base/management/commands/monthlydonationemail.py
+++ revolv/base/management/commands/monthlydonationemail.py
@@ -8,27 +8,28 @@ from revolv.lib.mailer import send_revolv_email
 
 class Command(BaseCommand):
     """
-    This handle function is the logic that is run when the command
-    python manage.py monthlydonationemail is typed into the command line.
+    This handle function is run when the command "python manage.py monthlydonationemail"
+    is typed into the command line.
 
-    It finds all users, goes through them, and sends a monthly donation
-    email to anyone who has donated in the past month
+    It iterates through all users, and sends a monthly donation email to any user
+    who has donated in the past month
     """
 
     def handle(self, *args, **options):
-        revolv_user_profiles = RevolvUserProfile.objects.all()
-        for revolv_user_profile in revolv_user_profiles:
-            donation_set = revolv_user_profile.payment_set.all()
-            donation_set = get_last_month_donations(donation_set)
-            if len(donation_set) > 0:
-                context = {}
-                user = revolv_user_profile.user
-                context['user'] = user
-                context['payments'] = donation_set
-                send_revolv_email(
-                    'monthly_donation_email',
-                    context, [user.email]
-                )
+        if datetime.date.today().day == 1:  # checks if the it is the first day of the month
+            revolv_user_profiles = RevolvUserProfile.objects.all()
+            for revolv_user_profile in revolv_user_profiles:
+                donation_set = revolv_user_profile.payment_set.all()
+                donation_set = get_last_month_donations(donation_set)
+                if len(donation_set) > 0:
+                    context = {}
+                    user = revolv_user_profile.user
+                    context['user'] = user
+                    context['payments'] = donation_set
+                    send_revolv_email(
+                        'monthly_donation_email',
+                        context, [user.email]
+                    )
 
 
 """
