36bd6cbf7f10fb25a660c3482186030b33788430
Eric Shen <email.ericshen@gmail.com>
2015-04-06 23:57:36 -0700

    Added tests for monthlydonationemail

--- revolv/lib/tests/test_mailer.py
+++ revolv/lib/tests/test_mailer.py
@@ -1,8 +1,14 @@
+import datetime
+
 import mock
 from django.core import mail
+from django.core.management import call_command
 from django.test import TestCase
+
 from revolv.base.models import RevolvUserProfile
 from revolv.lib.mailer import send_revolv_email
+from revolv.payments.models import Payment, PaymentType
+from revolv.project.models import Project
 
 
 class MailerTestCase(TestCase):
@@ -62,3 +68,77 @@ class MailerTestCase(TestCase):
         self.assertEqual(len(mail.outbox), 1)
         self.assertIn("a@a.com", mail.outbox[0].cc)
         self.assertIn("b@b.com", mail.outbox[0].cc)
+
+
+class MonthlyDonationEmailTestCase(TestCase):
+    def test_monthly_donation_email_no_donations(self):
+        """
+        Tests that if there are no donations, nothing will be sent
+        """
+        RevolvUserProfile.factories.base.create()
+        call_command('monthlydonationemail', override=True)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_monthly_donation_email(self):
+        """
+        Tests that with two donations in the past month from two different users,
+        the Revolv Mailer send two emails
+        """
+        # creates 2 users and 2 projects
+        user1, user2 = RevolvUserProfile.factories.base.create_batch(2)
+        project1, project2 = Project.factories.base.create_batch(2)
+
+        # gets a day from the last month
+        date_of_last_month = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+
+        # creates two payments and sets their created_at date to a date from last month
+        Payment.factories.base.create(
+            user=user1,
+            entrant=user1,
+            payment_type=PaymentType.objects.get_paypal(),
+            project=project1,
+            created_at=date_of_last_month
+        )
+
+        Payment.factories.base.create(
+            user=user2,
+            entrant=user2,
+            payment_type=PaymentType.objects.get_paypal(),
+            project=project2,
+            created_at=date_of_last_month
+        )
+
+        call_command('monthlydonationemail', override=True)
+        self.assertEqual(len(mail.outbox), 2)
+
+    def test_monthly_donation_email_no_donations_in_past_month(self):
+        """
+        Tests that with two donations from a year ago from two different users,
+        the Revolv Mailer send no emails
+        """
+        # creates 2 users and 2 projects
+        user1, user2 = RevolvUserProfile.factories.base.create_batch(2)
+        project1, project2 = Project.factories.base.create_batch(2)
+
+        # gets a day from last year
+        date_from_last_year = datetime.date.today() - datetime.timedelta(days=365)
+
+        # creates two payments and sets their created_at date to a date from last year
+        Payment.factories.base.create(
+            user=user1,
+            entrant=user1,
+            payment_type=PaymentType.objects.get_paypal(),
+            project=project1,
+            created_at=date_from_last_year
+        )
+
+        Payment.factories.base.create(
+            user=user2,
+            entrant=user2,
+            payment_type=PaymentType.objects.get_paypal(),
+            project=project2,
+            created_at=date_from_last_year
+        )
+
+        call_command('monthlydonationemail', override=True)
+        self.assertEqual(len(mail.outbox), 0)
