4a8224bc8fb7b8043d7a109d6290f0899f951d79
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-04-25 16:07:44 -0700

    changed project migration file 37

--- revolv/project/migrations/0037_auto_20150417_1512.py
+++ revolv/project/migrations/0037_auto_20150417_1512.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='project',
-            name='post_funding_updates',
-        ),
         migrations.AlterField(
             model_name='donationlevel',
             name='description',
