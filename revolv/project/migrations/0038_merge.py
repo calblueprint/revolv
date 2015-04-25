fb489c0e4c438b0a1c16a311ac42f52cc2b4be5c
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-04-25 15:56:25 -0700

    merged conflicting migrations

---
+++ revolv/project/migrations/0038_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('project', '0037_auto_20150414_0117'),
+        ('project', '0037_auto_20150417_1512'),
+    ]
+
+    operations = [
+    ]
