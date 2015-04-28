e3551337f0dd0c3aa4e7b42751719a6bb27e243e
Aleks Kamko <aykamko@gmail.com>
2015-04-27 18:46:47 -0700

    fixed duplicate/bad tests

--- revolv/project/tests/test_models.py
+++ revolv/project/tests/test_models.py
@@ -1,11 +1,8 @@
 import datetime
-import json
 
-import mock
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django_webtest import WebTest
 from revolv.base.models import RevolvUserProfile
-from revolv.lib.testing import TestUserMixin
 from revolv.payments.models import (AdminReinvestment, AdminRepayment, Payment,
                                     PaymentType)
 from revolv.project.models import Category, Project, ProjectUpdate
@@ -272,90 +269,3 @@ class ScrapeTest(TestCase):
     def test_scrape(self):
         result = scrape.delay()
         self.assertTrue(result.successful())
-
-
-class DonationAjaxTestCase(TestUserMixin, TestCase):
-    """
-    Test suite for AJAX payment donations for projects.
-    """
-    DONATION = 'donation/submit'
-
-    def setUp(self):
-        super(DonationAjaxTestCase, self).setUp()
-        self.send_test_user_login_request()
-        self.project = Project.factories.base.create(project_status=Project.ACTIVE)
-
-    def perform_valid_donation(self):
-        """
-        Utility method for performing a valid donation. Returns the response.
-        """
-        valid_donation = {
-            'csrfmiddlewaretoken': self.client.cookies['csrftoken'].value,
-            'type': 'visa',
-            'first_name': 'William',
-            'last_name': 'Taft',
-            'expire_month': 6,
-            'expire_year': 2020,
-            'cvv2': '00',
-            'number': '1234123412341234',
-            'amount': '10.00',
-        }
-        return self.client.post(
-            self.project.get_absolute_url() + self.DONATION,
-            data=valid_donation,
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-        )
-
-    # override payment charging because we only want to unit test that the
-    # ajax endpoints work (actually validating through Paypal takes a millenia)
-    @override_settings(ENABLE_PAYMENT_CHARGING=False)
-    def test_valid_donation(self):
-        """
-        Test valid donation via AJAX to /project/<pk>/donation/submit.
-        """
-        resp = self.perform_valid_donation()
-        self.assertEqual(resp.status_code, 200)
-        content = json.loads(resp.content)
-        self.assertIsNone(content.get('error'))
-        self.assertIsNotNone(self.project.donors.get(pk=self.test_user.pk))
-
-    @override_settings(ENABLE_PAYMENT_CHARGING=False)
-    @mock.patch('revolv.lib.mailer.EmailMultiAlternatives')
-    def test_post_donation_email(self, mock_mailer):
-        """
-        Tests whether a valid donation will send a revolv email
-        """
-        resp = self.perform_valid_donation()
-        self.assertEqual(resp.status_code, 200)
-        content = json.loads(resp.content)
-        self.assertIsNone(content.get('error'))
-        self.assertIsNotNone(self.project.donors.get(pk=self.test_user.pk))
-        self.assertTrue(mock_mailer.called)
-        # checks that the email address sent using mock mailer matches the user who donatred
-        args, kwargs = mock_mailer.call_args
-        self.assertEqual(kwargs['to'], ["john@example.com"])
-
-    def test_invalid_donation_ajax(self):
-        """
-        Tests that invalid donation appropriately errors with on
-        /donation/submit endpoint.
-        """
-        invalid_donation = {
-            'csrfmiddlewaretoken': self.client.cookies['csrftoken'].value,
-            'type': 'visa',
-            # 'first_name': '',
-            'last_name': 'Taft',
-            'expire_month': 6,
-            'expire_year': 2020,
-            'cvv2': '00',
-            'number': 'not a number',
-            'amount': '10.00',
-        }
-        resp = self.client.post(
-            self.project.get_absolute_url() + self.DONATION,
-            data=invalid_donation,
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-        )
-        content = json.loads(resp.content)
-        self.assertEquals(resp.status_code, 400)
-        self.assertIsNotNone(content['error'])
