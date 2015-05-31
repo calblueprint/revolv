1fd6f5993cd0057c2cd3def447fa0811955af6d2
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-05-31 18:36:12 -0500

    Add initial seed spec for project
    Only one project object is in there right now. Will do the other three
    shortly.

--- revolv/base/management/commands/seed.py
+++ revolv/base/management/commands/seed.py
@@ -1,9 +1,11 @@
+import datetime
 from exceptions import NotImplementedError
 from optparse import make_option
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from revolv.base.models import RevolvUserProfile
+from revolv.project.models import Project
 
 
 class SeedSpec(object):
@@ -70,8 +72,56 @@ class RevolvUserProfileSeedSpec(SeedSpec):
                 print "[Seed:Warning] Error in %s when trying to clear: %s" % (self.__class__.__name__, str(e))
 
 
+class ProjectSeedSpec(SeedSpec):
+    """
+    Database seed specification for revolv.project.models.Project
+
+    Creates 4 projects with various settings: Community Dairy, Community Dance Studio,
+    Educathing, and Fire Emblem.
+
+    TODO: Make this spec create projects that mirror the projects that RE-volv has already
+    completed.
+    """
+    studio = {
+        "funding_goal": 12000.0,
+        "title": "Power Community Dance Studio",
+        "tagline": "Dance forever, dance until dawn.",
+        "video_url": "https://www.youtube.com/watch?v=fzShzO2pk-E",
+        "org_name": "The Community Dance Studio",
+        "impact_power": 10.0,
+        "solar_url": "http://home.solarlog-web.net/1445.html",
+        "location": "2415 Bowditch St, Berkeley, CA 94704, United States",
+        "location_latitude": 37.8670289,
+        "location_longitude": -122.2561597,
+        "actual_energy": 0.0,
+        "end_date": datetime.date(2050, 10, 8),
+        "cover_photo": "covers/box.jpg",
+        "org_start_date": datetime.date(1995, 10, 9),
+        "mission_statement": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n",
+        "org_about": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n",
+        "internal_rate_return": 7.8
+    }
+    projects = [studio]
+
+    def seed(self):
+        ambassador = RevolvUserProfile.objects.get(user__username="ambassador")
+        Project.factories.active.create(ambassador=ambassador, **self.studio)
+        # Project.factories.completed.create(ambassador=ambassador, **self.dairy)
+        # Project.factories.proposed.create(ambassador=ambassador, **self.educathing)
+        # Project.factories.drafted.create(ambassador=ambassador, **self.emblem)
+
+    def clear(self):
+        for project in self.projects:
+            try:
+                Project.objects.get(tagline=project["tagline"]).delete()
+            except Project.DoesNotExist as e:
+                print "[Seed:Warning] Error in %s when trying to clear: %s" % (self.__class__.__name__, str(e))
+
+
 SPECS_TO_RUN = [
-    RevolvUserProfileSeedSpec()
+    RevolvUserProfileSeedSpec(),
+    ProjectSeedSpec()
 ]
 
 
