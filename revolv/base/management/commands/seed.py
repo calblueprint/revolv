99a95f56f5411a8434f5dc1d2acb9ca156c7f1ba
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-05-31 14:34:10 -0500

    Add in RevolvUserProfileSeedSpec
    This will be the new way to seed data for RevolvUserProfile. Going to
    test it presently. Also, set up a bit of infrastructure for defining how
    seeding works: basically, you can define a SeedSpec subclass which has
    two methods: seed() and clear(). Hopefully this will be a nice way to
    impelement different kinds of seeds.

--- revolv/base/management/commands/seed.py
+++ revolv/base/management/commands/seed.py
@@ -1,4 +1,77 @@
+from exceptions import NotImplementedError
+
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from revolv.base.models import RevolvUserProfile
+
+
+class SeedSpec(object):
+    """
+    A seed spec is an object which specifies a chunk of data that will be seeded into the
+    database when the seed command is run. This is an abstract class: children of it, for
+    example RevolvUserProfileSeedSpec or ProjectSeedSpec, must implement a few methods:
+
+    seed(): seeds the data into the database. This function may be indempotent (that is,
+        it may leave the database in the same state after being run the first time and
+        the second time), but the actual indempotence is up to the specific subclass of
+        SeedSpec.
+
+    clear(): clears the data that was seeded. If there was no data seeded, this function
+        should do nothing.
+    """
+
+    def seed(self):
+        raise NotImplementedError("Abstract SeedSpec class tried to call seed()")
+
+    def clear(self):
+        raise NotImplementedError("Abstract SeedSpec class tried to call clear()")
+
+
+class RevolvUserProfileSeedSpec(SeedSpec):
+    """
+    The database seed specification for revolv.base.models.RevolvUserProfile.
+
+    Creates three users: one donor, one ambassador, and one administrator. Logins are
+    donor/password, ambassador/password, administrator/password respectively.
+    """
+    usernames_to_clear = ["donor", "ambassador", "administrator"]
+
+    def seed(self):
+        RevolvUserProfile.objects.create_user(
+            username="donor",
+            email="donor@re-volv.org",
+            first_name="Joe",
+            last_name="Donor",
+            password="password"
+        )
+        RevolvUserProfile.objects.create_as_ambassador(
+            username="ambassador",
+            email="ambassador@re-volv.org",
+            first_name="Joe",
+            last_name="Ambassador",
+            password="password"
+        )
+        RevolvUserProfile.objects.create_user(
+            username="administrator",
+            email="administrator@re-volv.org",
+            first_name="Joe",
+            last_name="Admin",
+            password="password"
+        )
+
+    def clear(self):
+        for username in self.usernames_to_clear:
+            try:
+                user = User.objects.get(username=username)
+                RevolvUserProfile.objects.get(user=user).delete()
+                user.delete()
+            except User.DoesNotExist as e:
+                print "[Seed:Warning] Error in %s when trying to clear: %s" % (self.__class__.__name__, str(e))
+
+
+SPECS_TO_RUN = [
+    RevolvUserProfileSeedSpec()
+]
 
 
 class Command(BaseCommand):
@@ -15,5 +88,7 @@ class Command(BaseCommand):
         associated with them. This function fully expects signals to be enabled, and will
         create objects knowing that signals will be run on the creation of some of them.
         """
-        print "The seed command was run!"
-        pass  # right now do nothing
+        print "[Seed:Info] Seeding objects from %i seed specs..." % len(SPECS_TO_RUN)
+        for spec in SPECS_TO_RUN:
+            spec.seed()
+        print "[Seed:Info] Done!"
