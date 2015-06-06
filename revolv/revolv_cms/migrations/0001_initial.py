37177bb5c7587068d530afc1b97493568c7423e4
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-06-06 14:29:45 -0500

    Add initial migration for revolv_cms

---
+++ revolv/revolv_cms/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import wagtail.wagtailcore.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0015_add_more_verbose_names'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RevolvCustomPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('body', wagtail.wagtailcore.fields.RichTextField()),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
--- revolv/revolv_cms/models.py
+++ revolv/revolv_cms/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
-# Create your models here.
+
+class RevolvCustomPage(Page):
+    body = RichTextField()
+    search_name = "Custom Page"
+
+    indexed_fields = ('body', )
+
+
+RevolvCustomPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('date'),
+    FieldPanel('body', classname="full"),
+]
