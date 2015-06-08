e68c860c5e71bd16c5eced4c38a9ff5050751e8d
Eric Shen <email.ericshen@gmail.com>
2015-06-07 18:18:10 -0700

    Added dashboard object encapsulating drawing svg logic

--- revolv/static/js/base/dashboard.js
+++ revolv/static/js/base/dashboard.js
@@ -102,13 +102,13 @@ $(document).ready(function () {
         $(".dashboard-data-link.active").removeClass("active");
         $(this).addClass("active");
         // deletes repayment progress for the current project
-        deleteCurrentCircles();
+        dashboard.deleteCurrentCircles();
         $(".dashboard-data-section-current").removeClass("dashboard-data-section-current");
         var sectionToShow = $(".dashboard-data-section-" + $(this).data("section"));
         sectionToShow.addClass("dashboard-data-section-current");
 
         // draws repayment progress for the project we are switching to
-        draw();
+        dashboard.draw();
 
         // if we're in an orientation where we should collapse the sidebar, collapse it.
         if ($(window).width() < TABLET_PORTRAIT_BREAKPOINT) {
