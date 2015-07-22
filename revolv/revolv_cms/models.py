895c6f26a3a00e2d800565ff69f281ab8f2b09ee
Eric Shen <email.ericshen@gmail.com>
2015-07-21 20:53:52 -0700

    Formatted dollar values in project stats table

--- revolv/revolv_cms/models.py
+++ revolv/revolv_cms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
