bee2fcfb0b101df259508567cd80cb141ad064fc
Colin Copeland <copelco@caktusgroup.com>
2016-02-06 21:52:39 -0500

    switch email for testing

--- revolv/settings/base.py
+++ revolv/settings/base.py
@@ -18,7 +18,7 @@ DEBUG = True
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-    ('RE-volv Team', 'kmtracey@caktusgroup.com'),
+    ('RE-volv Team', 'copelco@caktusgroup.com'),
 )
 
 # Application definition
