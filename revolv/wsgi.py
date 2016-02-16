3547ec02c762f7e162a8dc324ddf72df4c1f3111
Karen Tracey <kmtracey@caktusgroup.com>
2016-02-16 09:41:44 -0500

    Remove Cling.
    Cling is a Heroku need, not needed for the current AWS deployment.

--- revolv/wsgi.py
+++ revolv/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "revolv.settings")
 
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
